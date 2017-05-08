f4d23f7f161387303f4999bee8354ac683d7acc4
治 张 <zzdxzhangzhi@msn.com>
2017-05-08 21:25:57 +1200

    first change

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Do you want me to write the two words?");
+            Console.WriteLine("Type y for yes; and n for no, then press <enter>");
+
+            string answer = Console.ReadLine();
+            if (answer == "y")
+                Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Bye Bye!");
+            Console.WriteLine("The result is {0}", 10 - 3 > 2 ? 0 : true ? 5 : 3);
         }
     }
 }
